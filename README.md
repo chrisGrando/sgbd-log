3bdf45b399bdf48d005cf834e57bacea786c513c
Christian Júlio de Liz Grando <christianjulio2010@gmail.com>
2022-11-02 16:11:25 -0300

    Base project files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # sgbd-log
 Trabalho Prático - BDII
+
+PostgreSQL JDBC Driver guide:<br>
+[www.enterprisedb.com](https://www.enterprisedb.com/postgres-tutorials/how-connect-postgres-database-using-eclipse-and-netbeans)
---
+++ src/main/java/app/Main.java
@@ -0,0 +1,15 @@
+/**
+*** @author chrisGrando
+*** Classe destinada somente para inicialização do sistema.
+*** Lógica do software fica localizada em AppStates.
+**/
+
+package app;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+}
